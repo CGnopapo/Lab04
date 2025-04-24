49f3a2c63535a2184b80c09b71af9b62fcfb98d1
Francisco Henriques <franciscolealhenriques@usp.br>
2025-04-24 10:47:36 -0300

    Add new version of lab04

--- Lab04/lab04.js
+++ Lab04/lab04.js
@@ -74,6 +74,20 @@ var gaPosicoes = [];
 var gaCores = [];
 
 var gObjetos = [];
+var gaIndices = [
+  1, 0, 3,
+  3, 2, 1,
+  2, 3, 7,
+  7, 6, 2,
+  3, 0, 4,
+  4, 7, 3,
+  6, 5, 1,
+  1, 2, 6,
+  4, 5, 6,
+  6, 7, 4,
+  5, 4, 0,
+  0, 1, 5
+];
 
 // ==================================================================
 // chama a main quando terminar de carregar a janela
@@ -127,9 +141,9 @@ function crieShaders() {
     gl.useProgram(gShader.program);
 
     // buffer dos vértices
-    var bufVertices = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufVertices);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(gaPosicoes), gl.STATIC_DRAW);
+    var bufIndices = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, bufIndices);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(gaIndices), gl.STATIC_DRAW);
 
     var aPosition = gl.getAttribLocation(gShader.program, "aPosition");
     gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, 0, 0);
@@ -154,7 +168,7 @@ function crieShaders() {
     gl.uniformMatrix4fv(gShader.uPerspective, false, flatten(gCtx.perspectiva));
 
     // calcula a matriz de transformação da camera, apenas 1 vez
-    let eye = vec3(10, 10, 10);
+    let eye = vec3(1.75, 1.75, 1.75);
     let at = vec3(0, 0, 0);
     let up = vec3(0, 1, 0);
     gCtx.vista = lookAt(eye, at, up);
@@ -171,17 +185,18 @@ function render() {
     //gl.cullFace(gl.BACK);
 
 
-    if (!gCtx.pause){
-        let num_vertices_ate_agr=0;
-        for (let i = 0; i < gObjetos.length; i++) {
+    if (gCtx.pause) return;
 
-            gObjetos[i].atualiza_model(1)
-            gl.uniformMatrix4fv(gShader.uModelView, false, flatten(gObjetos[i].model));
-            console.log(gObjetos[i].num_vertices)
-            gl.drawArrays(gl.TRIANGLES, num_vertices_ate_agr, gObjetos[i].num_vertices);
-            num_vertices_ate_agr += gObjetos[i].num_vertices;
+    let num_vertices_ate_agr=0;
 
-        }
+    for (let i = 0; i < gObjetos.length; i++) {
+
+        gObjetos[i].atualiza_model(1)
+        gl.uniformMatrix4fv(gShader.uModelView, false, flatten(gObjetos[i].model));
+        // console.log(gObjetos[i].num_vertices)
+        gl.drawElements(gl.TRIANGLES, gObjetos[i].num_vertices, gl.UNSIGNED_BYTE, 0);
+        num_vertices_ate_agr += gObjetos[i].num_vertices;
+
     }
 
 
@@ -241,6 +256,7 @@ function Cubo(translacao, rotacao, escala, vel_trans,vel_rota) {
     this.model = mult(T, mult(R, S));
     this.num_vertices = 0;
     crie_cubo(this)
+
     this.atualiza_model = function(delta){
 
         this.translacao = add(this.translacao, mult(delta, this.vel_trans));
@@ -298,120 +314,3 @@ function crie_cubo(objeto) {
 
     objeto.num_vertices = indices.length;
 };
-
-
-
-
-function Esfera(translacao, rotacao,escala,vel_trans,vel_rota) {
-
-    this.translacao = translacao;
-    this.rotacao = rotacao;
-    this.escala = escala;
-    this.vel_trans = vel_trans;
-    this.vel_rota = vel_rota;
-
-    let S = scale(escala[0],escala[1],escala[2]);
-    let rx = rotateX(rotacao[0]);
-    let ry = rotateY(rotacao[1]);
-    let rz = rotateZ(rotacao[2]);
-    let R = mult(rz, mult(ry, rx));
-    let T = translate(translacao[0],translacao[1],translacao[2]);
-
-    this.model = mult(T, mult(R, S));
-    this.num_vertices=0;
-    crieEsfera(2,this)
-
-    this.atualiza_model = function(delta){
-
-        this.translacao = add(this.translacao, mult(delta, this.vel_trans));
-
-        this.rotacao = add(this.rotacao, mult(delta, this.vel_rota));
-
-        let S = scale(escala[0], escala[1], escala[2]);
-        let rx = rotateX(rotacao[0]);
-        let ry = rotateY(rotacao[1]);
-        let rz = rotateZ(rotacao[2]);
-        let R = mult(rz, mult(ry, rx));
-        let T = translate(translacao[0], translacao[1], translacao[2]);
-        this.model = mult(T, mult(R, S));
-
-
-    }
-}
-
-function crieEsfera(ndivisoes = 2,objeto){
-    let vp = [
-        vec3(1.0, 0.0, 0.0),
-        vec3(0.0, 1.0, 0.0),
-        vec3(0.0, 0.0, 1.0),
-    ];
-
-    let vn = [
-        vec3(-1.0, 0.0, 0.0),
-        vec3(0.0, -1.0, 0.0),
-        vec3(0.0, 0.0, -1.0),
-    ];
-
-    let triangulo = [
-        [vp[0], vp[1], vp[2]],
-        [vp[0], vp[1], vn[2]],
-
-        [vp[0], vn[1], vp[2]],
-        [vp[0], vn[1], vn[2]],
-
-        [vn[0], vp[1], vp[2]],
-        [vn[0], vp[1], vn[2]],
-
-        [vn[0], vn[1], vp[2]],
-        [vn[0], vn[1], vn[2]],
-    ];
-
-    for (let i = 0; i < triangulo.length; i++) {
-        let a, b, c;
-        [a, b, c] = triangulo[i];
-        dividaTriangulo(a, b, c, ndivisoes,objeto);
-    }
-};
-
-function dividaTriangulo(a, b, c, ndivs,objeto) {
-    // Cada nível quebra um triângulo em 4 subtriângulos
-    // a, b, c em ordem mão direita
-    //    c
-    // a  b
-
-    // caso base
-    if (ndivs > 0) {
-        let ab = mix(a, b, 0.5);
-        let bc = mix(b, c, 0.5);
-        let ca = mix(c, a, 0.5);
-
-        ab = normalize(ab);
-        bc = normalize(bc);
-        ca = normalize(ca);
-
-        dividaTriangulo(a, ab, ca, ndivs - 1,objeto);
-        dividaTriangulo(b, bc, ab, ndivs - 1,objeto);
-        dividaTriangulo(c, ca, bc, ndivs - 1,objeto);
-        dividaTriangulo(ab, bc, ca, ndivs - 1,objeto);
-    }
-
-    else {
-        insiraTriangulo(a, b, c,objeto);
-    }
-};
-
-function insiraTriangulo(a, b, c,objeto) {
-
-    gaPosicoes.push(a);
-    gaPosicoes.push(b);
-    gaPosicoes.push(c);
-    objeto.num_vertices = objeto.num_vertices+3;
-    let cor = COR_ESFERA;
-
-    if (!COR_SOLIDA) cor = sorteieCorRGBA();
-    gaCores.push(cor);
-    if (!COR_SOLIDA) cor = sorteieCorRGBA();
-    gaCores.push(cor);
-    if (!COR_SOLIDA) cor = sorteieCorRGBA();
-    gaCores.push(cor);
-}
